88e6ad77d76efeee0d3fcd1d64cdf8a091999f2c
Joseph Ryan <xazaviar@gmail.com>
2017-03-20 15:52:24 -0400

    Initial Code upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author Xazav
+ */
+public class Main {
+    
+    //Static info
+    String[] dataType = {"edibility","cap-shape","cap-surface","cap -color"};
+    
+    public static void main(String[] args){
+        
+    }
+}
